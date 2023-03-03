0aea080b166f70479c70e20e1621b58968f30375
uralbi <ural46@gmail.com>
2023-03-03 12:31:45 -0800

    Custom User Model, and other models

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,102 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.utils.translation import gettext_lazy as _
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 
-# Create your models here.
+
+class IforumUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("Email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        _("email address"),
+        unique=True,
+    )
+
+    objects = IforumUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+class Tag(models.Model):
+  value = models.SlugField(max_length=100)
+
+  def __str__(self):
+    return self.value
+
+
+# class Gallery(models.Model):
+#     image = models.ImageField(upload_to='gallery/%Y/%m/%d',
+#                               blank=True)
+#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+#     object_id = models.PositiveIntegerField(db_index=True)
+#     content_object = GenericForeignKey("content_type", "object_id")
+#     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+
+class Comment(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    content = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField(db_index=True)
+    content_object = GenericForeignKey("content_type", "object_id")
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(blank=True, null=True)
+    title = models.TextField(max_length=100)
+    slug = models.SlugField()
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name='posts')
+    comments = GenericRelation(Comment)
+    # gallery = GenericRelation(Gallery)
+
+
+class AuthorProfile(models.Model):
+  user = models.OneToOneField(
+      settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile"
+  )
+  bio = models.TextField()
+#   pic = GenericRelation(Gallery)
+
+  def __str__(self):
+      return f"{self.__class__.__name__} object for {self.user}"
